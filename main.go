350b68cbd11794097d04758531df84308b09135c
1Dice <23101388+1DIce@users.noreply.github.com>
2024-09-30 16:13:27 +0200

    Setup basic REPL functionality

--- main.go
+++ main.go
@@ -1,7 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error{
+  commands:= getCliCommands()
+
+  fmt.Println("")
+  fmt.Println("Welcome to the Pokedex!")
+  fmt.Println("Usage:")
+  fmt.Println("")
+
+  for _, command:= range commands{
+    fmt.Printf("%s:\t %s\n", command.name, command.description)
+  }
+  fmt.Println("")
+  return nil
+}
+
+func commandExit() error{
+  os.Exit(0)
+  return nil
+}
+
+
+func getCliCommands() map[string]cliCommand{
+
+return map[string]cliCommand{
+    "help": {
+        name:        "help",
+        description: "Displays a help message",
+        callback:    func() error{return commandHelp()},
+    },
+    "exit": {
+        name:        "exit",
+        description: "Exit the Pokedex",
+        callback:    func() error{return commandExit()},
+    },
+}
+}
 
 func main(){
-  fmt.Println("hello world")
+  inputScanner:= bufio.NewScanner(os.Stdin)
+
+  availableCommands:= getCliCommands()
+
+  for{
+    fmt.Print("Pokedex > ")
+    hasMoreTokens := inputScanner.Scan()
+    if hasMoreTokens          {
+      // TODO handle this later?!
+    }
+
+    input:= inputScanner.Text()
+
+    command, ok := availableCommands[input]
+    if !ok{
+      fmt.Printf("'%s' is not a valid command", input)
+      continue;
+    }
+
+    err:= command.callback()
+    if err!= nil{
+      fmt.Printf("Error during command execution %v", err)
+    }
+
+  }
+
+
 }

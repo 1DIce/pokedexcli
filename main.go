e6c1f60e041953778b788f6a99b088740dcda703
1Dice <23101388+1DIce@users.noreply.github.com>
2024-09-30 16:25:09 +0200

    auto format code

--- main.go
+++ main.go
@@ -6,76 +6,71 @@ import (
 	"os"
 )
 
-
 type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
 }
 
-func commandHelp() error{
-  commands:= getCliCommands()
+func commandHelp() error {
+	commands := getCliCommands()
 
-  fmt.Println("")
-  fmt.Println("Welcome to the Pokedex!")
-  fmt.Println("Usage:")
-  fmt.Println("")
+	fmt.Println("")
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
 
-  for _, command:= range commands{
-    fmt.Printf("%s:\t %s\n", command.name, command.description)
-  }
-  fmt.Println("")
-  return nil
+	for _, command := range commands {
+		fmt.Printf("%s:\t %s\n", command.name, command.description)
+	}
+	fmt.Println("")
+	return nil
 }
 
-func commandExit() error{
-  os.Exit(0)
-  return nil
+func commandExit() error {
+	os.Exit(0)
+	return nil
 }
 
-
-func getCliCommands() map[string]cliCommand{
-
-return map[string]cliCommand{
-    "help": {
-        name:        "help",
-        description: "Displays a help message",
-        callback:    func() error{return commandHelp()},
-    },
-    "exit": {
-        name:        "exit",
-        description: "Exit the Pokedex",
-        callback:    func() error{return commandExit()},
-    },
-}
+func getCliCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    func() error { return commandHelp() },
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    func() error { return commandExit() },
+		},
+	}
 }
 
-func main(){
-  inputScanner:= bufio.NewScanner(os.Stdin)
+func main() {
+	inputScanner := bufio.NewScanner(os.Stdin)
 
-  availableCommands:= getCliCommands()
+	availableCommands := getCliCommands()
 
-  for{
-    fmt.Print("Pokedex > ")
-    hasMoreTokens := inputScanner.Scan()
-    if hasMoreTokens          {
-      // TODO handle this later?!
-    }
+	for {
+		fmt.Print("Pokedex > ")
+		hasMoreTokens := inputScanner.Scan()
+		if hasMoreTokens {
+			// TODO handle this later?!
+		}
 
-    input:= inputScanner.Text()
+		input := inputScanner.Text()
 
-    command, ok := availableCommands[input]
-    if !ok{
-      fmt.Printf("'%s' is not a valid command", input)
-      continue;
-    }
+		command, ok := availableCommands[input]
+		if !ok {
+			fmt.Printf("'%s' is not a valid command!\n", input)
+			continue
+		}
 
-    err:= command.callback()
-    if err!= nil{
-      fmt.Printf("Error during command execution %v", err)
-    }
+		err := command.callback()
+		if err != nil {
+			fmt.Printf("Error during command execution %v\n", err)
+		}
 
-  }
-
-
+	}
 }
